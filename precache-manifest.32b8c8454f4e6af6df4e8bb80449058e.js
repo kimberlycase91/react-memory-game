959924bf493e3c6e9ddb7633c9d0659fd93e0094
kimberlycase91 <kimberlycase91@gmail.com>
2020-04-18 10:34:53 -0700

    Updates

---
+++ precache-manifest.32b8c8454f4e6af6df4e8bb80449058e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918347c02fa918a1890c5764229a57ce",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "2b4bb6920fd4b2ee2082",
+    "url": "/react-memory-game/static/css/main.5d4930c8.chunk.css"
+  },
+  {
+    "revision": "6182c3aed6512960b596",
+    "url": "/react-memory-game/static/js/2.d5d5a05e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-memory-game/static/js/2.d5d5a05e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4bb6920fd4b2ee2082",
+    "url": "/react-memory-game/static/js/main.fb171cb8.chunk.js"
+  },
+  {
+    "revision": "5cccf15bf10c39f7112d",
+    "url": "/react-memory-game/static/js/runtime-main.4cdd9c43.js"
+  },
+  {
+    "revision": "5f26da628c7f66b4aae097e1962b9dd1",
+    "url": "/react-memory-game/static/media/pink_rice.5f26da62.png"
+  }
+]);
